1803d7664708c1536d9e1f397e0efcf1ec951c13
mvansteenhuyse <matthis.vansteenhuyse@expertum.net>
2024-07-11 15:17:24 +0200

    had typo in path

--- Powershell/main_setup.ps1
+++ Powershell/main_setup.ps1
@@ -1,22 +1,5 @@
-# Get the current script directory
-$currentDir = Get-Location
-
-Write-Output "Setting up VSCode and extensions"
-
-# Construct the absolute path for the script
-$vscodeScriptPath = Join-Path $currentDir "sub_setup_script\install_vscode_and_extensions.ps1"
-if (Test-Path $vscodeScriptPath) {
-    & $vscodeScriptPath
-} else {
-    Write-Error "Script not found: $vscodeScriptPath"
-}
+Write-Output "Setting up VScode and extensions"
+.\sub_setup_scripts\install_vscode_and_extensions.ps1
 
 Write-Output "Setting up policies & installing WSL2 Ubuntu LTS after windows restart"
-
-# Construct the absolute path for the script
-$wslScriptPath = Join-Path $currentDir "sub_setup_script\wsl_part1.ps1"
-if (Test-Path $wslScriptPath) {
-    & $wslScriptPath
-} else {
-    Write-Error "Script not found: $wslScriptPath"
-}
+.\sub_setup_scripts\wsl_part1.ps1
