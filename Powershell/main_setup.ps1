9a1348b550645db6d97117a3969dafe3e831507c
mvansteenhuyse <matthis.vansteenhuyse@expertum.net>
2024-07-12 11:44:17 +0200

    added a bit of output

--- Powershell/main_setup.ps1
+++ Powershell/main_setup.ps1
@@ -1,10 +1,10 @@
 # Paths to witness files
 $witnessDir = "C:\\Witness"
 $witnessConditions = @(
-    @{ Path = "$witnessDir\\install_vscode_and_extensions.txt"; Condition = {-Not (Test-Path "$witnessDir\\install_vscode_and_extensions.txt")}; Script = ".\\install_vscode_and_extensions.ps1" },
-    @{ Path = "$witnessDir\\wsl_part1.txt"; Condition = { (Test-Path "$witnessDir\\install_vscode_and_extensions.txt") -and (-Not (Test-Path "$witnessDir\\wsl_part1.txt")) }; Script = ".\\wsl_part1.ps1" },
-    @{ Path = "$witnessDir\\wsl_part2.txt"; Condition = { (Test-Path "$witnessDir\\wsl_part1.txt") -and (-Not (Test-Path "$witnessDir\\wsl_part2.txt")) }; Script = ".\\wsl_part2.ps1" },
-    @{ Path = "$witnessDir\\setup_wsl_ubuntu.txt"; Condition = { (Test-Path "$witnessDir\\wsl_part1.txt") -and (Test-Path "$witnessDir\\wsl_part2.txt") -and (-Not (Test-Path "$witnessDir\\setup_wsl_ubuntu.txt")) }; Script = ".\\setup_wsl_ubuntu.ps1" }
+    @{Name = "VSCODE" ; ScriptStep = '1'; Path = "$witnessDir\\install_vscode_and_extensions.txt"; Condition = {-Not (Test-Path "$witnessDir\\install_vscode_and_extensions.txt")}; Script = ".\\sub_setup_scripts\\install_vscode_and_extensions.ps1" },
+    @{Name = "WSL PART 1" ; ScriptStep = '2'; Path = "$witnessDir\\wsl_part1.txt"; Condition = { (Test-Path "$witnessDir\\install_vscode_and_extensions.txt") -and (-Not (Test-Path "$witnessDir\\wsl_part1.txt")) }; Script = ".\\sub_setup_scripts\\wsl_part1.ps1" },
+    @{Name = "WSL PART 2" ; ScriptStep = '3'; Path = "$witnessDir\\wsl_part2.txt"; Condition = { (Test-Path "$witnessDir\\wsl_part1.txt") -and (-Not (Test-Path "$witnessDir\\wsl_part2.txt")) }; Script = ".\\sub_setup_scripts\\wsl_part2.ps1" },
+    @{Name = "WSL UBUNTU CONFIG" ; ScriptStep = '4'; Path = "$witnessDir\\setup_wsl_ubuntu.txt"; Condition = { (Test-Path "$witnessDir\\wsl_part1.txt") -and (Test-Path "$witnessDir\\wsl_part2.txt") -and (-Not (Test-Path "$witnessDir\\setup_wsl_ubuntu.txt")) }; Script = ".\\sub_setup_scripts\\setup_wsl_ubuntu.ps1" }
 )
 
 # Create witness directory if it doesn't exist
@@ -15,6 +15,7 @@ if (-Not (Test-Path $witnessDir)) {
 # Iterate through the conditions and execute the corresponding scripts
 foreach ($condition in $witnessConditions) {
     if (& $condition.Condition) {
+        Write-Output $condition.Name + ": this is step[" + $condition.ScriptStep +"]"
         & $condition.Script
     }
 }
