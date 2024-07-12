483b51e66fbd7a6c7437d1ef74628ffb82b12293
mvansteenhuyse <matthis.vansteenhuyse@expertum.net>
2024-07-12 11:14:05 +0200

    Updated main script so it checks for witness files, these files then detirmen the next steps that run

--- Powershell/main_setup.ps1
+++ Powershell/main_setup.ps1
@@ -1,5 +1,30 @@
-Write-Output "Setting up VScode and extensions"
-.\sub_setup_scripts\install_vscode_and_extensions.ps1
+# Paths to witness files
+$witnessDir = "C:\Witness"
+$witnessFiles = @(
+    "$witnessDir\setup_wsl_ubuntu.txt",
+    "$witnessDir\wsl_part1.txt",
+    "$witnessDir\wsl_part2.txt",
+    "$witnessDir\install_vscode_and_extensions.txt"
+)
 
-Write-Output "Setting up policies & installing WSL2 Ubuntu LTS after windows restart"
-.\sub_setup_scripts\wsl_part1.ps1
+# Create witness directory if it doesn't exist
+if (-Not (Test-Path $witnessDir)) {
+    New-Item -ItemType Directory -Path $witnessDir
+}
+
+# Check and run each script based on the presence of its witness file
+if (-Not (Test-Path $witnessFiles[0])) {
+    .\setup_wsl_ubuntu.ps1
+}
+
+if (-Not (Test-Path $witnessFiles[1])) {
+    .\wsl_part1.ps1
+}
+
+if (-Not (Test-Path $witnessFiles[2])) {
+    .\wsl_part2.ps1
+}
+
+if (-Not (Test-Path $witnessFiles[3])) {
+    .\install_vscode_and_extensions.ps1
+}
